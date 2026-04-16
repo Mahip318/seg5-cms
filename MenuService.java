553e0ba30a2f243d99eeba9764b4e29c25f85306
Luis Aparicio <lapar009@fiu.edu>
2026-04-16 18:25:10 -0400

    Update MenuService.java

--- MenuService.java
+++ MenuService.java
@@ -1,2 +1,51 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class MenuService {
+    private List<MenuItem> database = new ArrayList<>();
+
+    public MenuService() {
+        //Seed menu items (same as OrderService for consistency)
+        database.add(new MenuItem("Taro Milk Tea", 5.50, 10));
+        database.add(new MenuItem("Matcha Latte", 6.00, 5));
+        database.add(new MenuItem("Thai Ice Tea", 4.75, 15));
+        database.add(new MenuItem("Brown Sugar Boba", 6.50, 8));
+    }
+
+    //Retrieve full menu
+    public List<MenuItem> getMenu() {
+        return database;
+    }
+
+    //Display menu
+    public void displayMenu() {
+        for (int i = 0; i < database.size(); i++) {
+            MenuItem item = database.get(i);
+            System.out.println(i + ". " + item.getName() +
+                    " - $" + item.getPrice() +
+                    " (Stock: " + item.getStock() + ")");
+        }
+    }
+
+    //Add a new item (Manager)
+    public void addItem(String name, double price, int stock) {
+        database.add(new MenuItem(name, price, stock));
+        System.out.println("Item added: " + name);
+    }
+
+    //Update an item (Manager)
+    public void updateItem(int index, String name, double price, int stock) {
+        MenuItem item = database.get(index);
+        item.setName(name);
+        item.setPrice(price);
+        item.setStock(stock);
+
+        System.out.println("Item updated: " + name);
+    }
+
+    //Delete an item (Manager)
+    public void deleteItem(int index) {
+        MenuItem removed = database.remove(index);
+        System.out.println("Item removed: " + removed.getName());
+    }
 }
