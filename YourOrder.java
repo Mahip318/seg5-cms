837faa346144df821e5e3f029d057bb7d7113272
Luis Aparicio <lapar009@fiu.edu>
2026-04-16 18:51:33 -0400

    Update YourOrder.java

--- YourOrder.java
+++ YourOrder.java
@@ -1,10 +1,35 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class YourOrder {
-    private List<OrderItem> items;
+
+    private List<OrderItem> items = new ArrayList<>();
     private double total;
 
-    public void calculateTotal() {
-        total = items.stream()
-                .mapToDouble(item -> item.getPrice() * item.getQuantity())
-                .sum();
-        }
+    public void addItem(OrderItem item) {
+        items.add(item);
     }
+
+    public List<OrderItem> getItems() {
+        return items;
+    }
+
+    public double calculateTotal() {
+        total = 0;
+        for (OrderItem item : items) {
+            total += item.getPrice() * item.getQuantity();
+        }
+        return total;
+    }
+
+    public void displayOrder() {
+        for (OrderItem item : items) {
+            System.out.println(
+                item.getItem().getName() +
+                " x" + item.getQuantity() +
+                " = $" + (item.getPrice() * item.getQuantity())
+            );
+        }
+        System.out.println("TOTAL: $" + calculateTotal());
+    }
+}
