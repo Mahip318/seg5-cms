afefd214e1016efce903e666aa62b262ad6cdbab
Luis Aparicio <lapar009@fiu.edu>
2026-04-16 18:28:25 -0400

    Update MenuItem.java

--- MenuItem.java
+++ MenuItem.java
@@ -13,6 +13,16 @@ public class MenuItem {
     public String getName() { return name; }
     public double getPrice() { return price; }
     public int getStock() { return stock; }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    public void setPrice(double price) {
+        this.price = price;
+    }
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
     
     public void reduceStock(int qty) { this.stock -= qty; }
 }
